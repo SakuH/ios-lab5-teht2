f32f43024b5d7ea1af89c3f674c5ef32eb62a3cf
sakuh <saku.huuha@gmail.com>
2019-09-24 16:11:57 +0300

    Initial Commit

---
+++ Ios_Labra5_teht2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ios_Labra5_teht2
+//
+//  Created by Saku Huuha on 24/09/2019.
+//  Copyright © 2019 Saku Huuha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
